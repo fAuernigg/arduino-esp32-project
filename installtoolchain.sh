6bf4e4e4cdc908876074c9b9d3b7541772959953
mrxa <franz.auernigg@gmail.com>
2019-08-29 00:23:53 +0200

    installtoolchain.sh: simplify toolchain installation

--- installtoolchain.sh
+++ installtoolchain.sh
@@ -19,77 +19,18 @@ fi
 
 export PATH=$PATH:$spath/xtensa-esp32-elf/bin
 
-if [ ! -d "crosstool-NG" ] ; then
-	git clone -b xtensa-1.22.x https://github.com/espressif/crosstool-NG.git
-	if [ $? -ne 0 ] ; then echo "error git clone crosstool-NG" ; exit 1; fi
-
-	cd crosstool-NG
-
-	# fix file download problem:
-	mkdir -p .build/tarballs
-	cp ../expat-2.1.0.tar.gz .build/tarballs/expat-2.1.0.tar.gz
-
-	if [ $? -ne 0 ] ; then echo "error cd crosstool-NG" ; exit 1; fi
-	./bootstrap
-	if [ $? -ne 0 ] ; then echo "error ./bootstrap" ; exit 1; fi
-
-
-	./configure --prefix=$PWD
-	if [ $? -ne 0 ] ; then echo "error ./configure --prefix=$PWD ctg" ; exit 1; fi
-
-	make install
-	if [ $? -ne 0 ] ; then echo "error ./ct-ng make install" ; exit 1; fi
-
-	./ct-ng xtensa-esp32-elf
-	if [ $? -ne 0 ] ; then echo "error ./ct-ng xtensa-esp32-elf" ; exit 1; fi
-
-	./ct-ng build
-	if [ $? -ne 0 ] ; then echo "error ./ct-ng build" ; exit 1; fi
-
-	chmod -R u+w builds/xtensa-esp32-elf
-	if [ $? -ne 0 ] ; then echo "error chmod -R u+w builds/xtensa-esp32-elf" ; exit 1; fi
-fi
-
 cd $spath
-if [ $? -ne 0 ] ; then echo "error cd ~/$spath" ; exit 1; fi
-
-if [ ! -d "esp-idf" ] ; then
-	git clone --recursive https://github.com/espressif/esp-idf.git
-	if [ $? -ne 0 ] ; then echo "error  git clone esp-idf.git" ; exit 1; fi
-	git checkout release/v3.2
-else
-	cd esp-idf
-	git pull
-	git checkout release/v3.2
-	cd ..
-fi
+git submodule update --init
 
 cd esp-idf
-if [ $? -ne 0 ] ; then echo "error cd esp-idf" ; exit 1; fi
-
+if [ $? -ne 0 ] ; then echo "error apt install" ; exit 1; fi
 git submodule update --init
-if [ $? -ne 0 ] ; then echo "error git submodule update --init esp-idf failed" ; exit 1; fi
 
 
-cd $spath/newproject/
-if [ ! -d "components" ] ; then
-	mkdir components
-fi
-cd components
+cd "$spath"
+if [ $? -ne 0 ] ; then echo "error apt install" ; exit 1; fi
+python -m pip install --user -r esp-idf/requirements.txt
 
-if [ ! -d "arduino" ] ; then
-	# install arduino-esp32 as component of esp-idf
-	git clone https://github.com/espressif/arduino-esp32.git arduino
-	if [ $? -ne 0 ] ; then echo "error cloning arduino-esp32" ; exit 11 ; fi
-fi
-
-cd arduino
-if [ $? -ne 0 ] ; then echo "error changing into arduino compoment" ; exit 11 ; fi
-git submodule update --init --recursive
-if [ $? -ne 0 ] ; then echo "error update arduino-esp32 submodules" ; exit 11 ; fi
-
-
-cd $spath
 
 echo "----------------------"
 echo "installtoolchain done"
