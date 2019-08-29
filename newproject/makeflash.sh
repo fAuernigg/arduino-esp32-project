1a89a83983b341aa90dfc241400af2627345b242
Franz Auernigg <f.auernigg@commend.com>
2019-08-29 10:54:33 +0200

    makeflash.sh: remove settting crosstool path

--- newproject/makeflash.sh
+++ newproject/makeflash.sh
@@ -5,7 +5,7 @@ spath=`pwd`
 cd ..
 
 export IDF_PATH=`pwd`/esp-idf
-export PATH=$PATH:`pwd`/crosstool-NG/builds/xtensa-esp32-elf:`pwd`/xtensa-esp32-elf/bin:`pwd`/crosstool-NG/builds/xtensa-esp32-elf:`pwd`/xtensa-esp32-elf/bin
+export PATH=$PATH:`pwd`/xtensa-esp32-elf/bin:`pwd`/../xtensa-esp32-elf/bin:`pwd`/xtensa-esp32-elf/bin
 
 cd $spath
 make flash monitor 2>&1
